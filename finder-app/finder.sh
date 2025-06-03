4a0633d4effc84e29cfb7442e2c076c3c5c20b0a
Sergey Tolmachev <tetron@gmail.com>
2025-06-03 09:34:43 +0100

    add finder.sh and writer.sh

---
+++ finder-app/finder.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+filesdir=$1
+searchstr=$2
+
+if [[ $# -lt 2 ]]; then
+    echo "One of the input prameters is missing..."
+    exit 1
+fi
+
+if [[ ! -d ${filesdir} ]]; then
+    echo "${filesdir} directory doesn't exist..."
+    exit 1
+fi
+
+number_of_files=$(find "${filesdir}" -type f | wc -l)
+number_of_lines=$(grep -R "${searchstr}" "${filesdir}" | wc -l)
+
+echo "The number of files are ${number_of_files} and the number of matching lines are ${number_of_lines}" 
---
+++ finder-app/writer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+writefile=$1
+writestr=$2
+
+if [[ $# -lt 2 ]]; then
+    echo "One of the input prameters is missing..."
+    exit 1
+fi
+
+dir_name=$(dirname "${writefile}")
+mkdir -p "${dir_name}"
+
+if [[ $? -eq 1 ]]; then
+    echo "Error creating a directory"
+    exit 1
+fi
+
+touch "${writefile}"
+if [[ $? -eq 1 ]]; then
+    echo "Error creating a file"
+    exit 1
+fi
+
+echo "${writestr}" > "${writefile}" 
